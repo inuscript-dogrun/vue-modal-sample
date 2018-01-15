fd47a6f4ae11db6d44b3b3a58b2bf3ab07bbcab6
inuscript <inuscript@users.noreply.github.com>
2018-01-15 22:41:18 +0900

    add

---
+++ src/HelloModal.vue
@@ -0,0 +1,14 @@
+<template>
+  <modal>
+    Hello!
+  </modal>
+</template>
+
+<script>
+import Vue from "vue"
+import VModal from "vue-js-modal"
+Vue.use(VModal)
+
+export default {
+}
+</script>
---
+++ src/ModalLink.vue
@@ -0,0 +1,6 @@
+<template>
+  <button>
+    Open Modal
+  </button>
+</template>
+
