f38fc054df37d6284e2deca835af4774ec2bb21c
inuscript <inuscript@users.noreply.github.com>
2018-01-15 23:36:04 +0900

    add

--- src/HelloModal.vue
+++ src/HelloModal.vue
@@ -1,9 +1,11 @@
 <template>
-  <modal name="hello-modal">
-    <div class="container">
-      <slot></slot>
-    </div>
-  </modal>
+  <div :style="{display: ready ? 'block' : 'none'}">
+    <modal name="hello-modal">
+      <div class="container">
+        Hello!
+      </div>
+    </modal>
+  </div>
 </template>
 
 <script>
@@ -14,7 +16,14 @@ import { ModalEvent } from "./ModalEvent"
 Vue.use(VModal)
 
 export default {
+  data(){
+    return {
+      ready: false
+    }
+  },
   mounted(){
+    console.log(this.$slots)
+    this.ready = true
     ModalEvent.$on("openModal", () => {
       this.$modal.show("hello-modal")
     })
